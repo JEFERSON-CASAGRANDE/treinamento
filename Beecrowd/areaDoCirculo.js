65964cc45ac061dd5bdbac7294f4e3ff9d63c50d
Jeferson Casagrande <jefersoncasagrandedesouza@gmail.com>
2023-01-06 13:33:31 -0300

    feat: Beecrowd

---
+++ Beecrowd/areaDoCirculo.js
@@ -0,0 +1,7 @@
+let n = 3.14159;
+let raio = parseFloat(input);
+let area = n * (raio * raio);
+
+
+
+console.log(`A=${area.toFixed(4)}`)
---
+++ Beecrowd/somaSimples.js
@@ -0,0 +1,5 @@
+let A = parseInt(lines.shift())
+let B = parseInt(lines.shift())
+let soma = A + B
+
+console.log(`SOMA = ${soma}`)
